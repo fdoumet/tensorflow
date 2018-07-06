e77a21ef5f71e2c3c0d5b5bbacd505984da10aad
Francis <fdoumet@gmail.com>
2018-07-05 18:00:45 -0700

    temp commit

---
+++ Encore-ios/Encore/Encore/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Encore
+//
+//  Created by Francis Doumet on 7/4/18.
+//  Copyright © 2018 MLVX. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
